a71d0e548d5d66528065fa217d267443554f8b3e
Mark Takacs <takacs.mark@icloud.com>
2023-12-20 12:27:36 +0100

    main page with hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+type HeroSectionProps = {
+  topNavigationScrollPX: number;
+};
+
+const Hero: React.FC<HeroSectionProps> = ({ topNavigationScrollPX }) => {
+  return (
+    <section className="dark:bg-gray-900">
+      <Navigation topNavigationScrollPX={topNavigationScrollPX} />
+
+      <div className="relative isolate overflow-hidden">
+        <img
+          src="/images/Hero.jpg"
+          alt=""
+          className="absolute inset-0 -z-10 h-full w-full object-cover opacity-60 xl:object-contain xl:object-right"
+        />
+        <img
+          src="/images/Hero2.jpg"
+          alt=""
+          className="invisible absolute inset-0 -z-10 h-full w-full object-cover opacity-60 -scale-x-100 transform-gpu 2xl:visible xl:object-contain xl:object-right"
+        />
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-2xl py-60 sm:py-[444px] min-h-screen">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight dark:text-white sm:text-6xl">
+              AI for Good
+            </h1>
+            <p className="mt-6 text-lg leading-8 dark:text-gray-300">
+              Highlighting the importance of mental health and compassion
+              through AI generated illustrations. Building solutions to build a
+              better world. Leading transformation programs with empathy.
+            </p>
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
