613d1bfb4693c5418f444dccd654c99b12d450ec
Mark Takacs <takacs.mark@icloud.com>
2025-02-14 14:06:27 +0000

    ïnitial

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,153 @@
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+
+export default function Example() {
+  return (
+    <Disclosure as="nav" className="bg-black">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex items-center">
+            <div className="shrink-0">
+              <div>takacsmark.com</div>
+            </div>
+            <div className="hidden sm:ml-6 sm:block"></div>
+          </div>
+          <div className="hidden sm:ml-6 sm:block">
+            <div className="flex space-x-4 font-ibm">
+              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+              <a
+                href="#"
+                className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+              >
+                About Márk Takács
+              </a>
+              <a
+                href="#"
+                className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              >
+                Links
+              </a>
+              <a
+                href="#"
+                className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              >
+                Savalera
+              </a>
+              <a
+                href="#"
+                className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              >
+                Calendar
+              </a>
+            </div>
+          </div>
+          <div className="-mr-2 flex sm:hidden">
+            {/* Mobile menu button */}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="block size-6 group-data-[open]:hidden"
+              />
+              <XMarkIcon
+                aria-hidden="true"
+                className="hidden size-6 group-data-[open]:block"
+              />
+            </DisclosureButton>
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+          <DisclosureButton
+            as="a"
+            href="#"
+            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+          >
+            Dashboard
+          </DisclosureButton>
+          <DisclosureButton
+            as="a"
+            href="#"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+          >
+            Team
+          </DisclosureButton>
+          <DisclosureButton
+            as="a"
+            href="#"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+          >
+            Projects
+          </DisclosureButton>
+          <DisclosureButton
+            as="a"
+            href="#"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+          >
+            Calendar
+          </DisclosureButton>
+        </div>
+        <div className="border-t border-gray-700 pb-3 pt-4">
+          <div className="flex items-center px-5">
+            <div className="shrink-0">
+              <img
+                alt=""
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                className="size-10 rounded-full"
+              />
+            </div>
+            <div className="ml-3">
+              <div className="text-base font-medium text-white">Tom Cook</div>
+              <div className="text-sm font-medium text-gray-400">
+                tom@example.com
+              </div>
+            </div>
+            <button
+              type="button"
+              className="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            >
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">View notifications</span>
+              <BellIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-3 space-y-1 px-2">
+            <DisclosureButton
+              as="a"
+              href="#"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+            >
+              Your Profile
+            </DisclosureButton>
+            <DisclosureButton
+              as="a"
+              href="#"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+            >
+              Settings
+            </DisclosureButton>
+            <DisclosureButton
+              as="a"
+              href="#"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+            >
+              Sign out
+            </DisclosureButton>
+          </div>
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  );
+}
