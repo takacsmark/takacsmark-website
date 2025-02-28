3567d65a49635b3cb2211edf8a4973ecd440628e
Mark Takacs <takacs.mark@icloud.com>
2025-02-28 14:38:04 +0100

    external links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,18 @@
 
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  Bars3Icon,
+  XMarkIcon,
+  ArrowTopRightOnSquareIcon,
+} from "@heroicons/react/24/outline";
 import siteLogo from "../assets/takacsmark-logo-color.svg";
 
 const navigation = [
-  { name: "Home", href: "/" },
-  { name: "Links", href: "https://linktr.ee/takacsmark" },
-  { name: "Savalera", href: "https://savalera.com" },
-  { name: "About Mark", href: "/about" },
+  { name: "Home", href: "/", type: "internal" },
+  { name: "Links", href: "https://linktr.ee/takacsmark", type: "external" },
+  { name: "Savalera", href: "https://savalera.com", type: "external" },
+  { name: "About Mark", href: "/about", type: "internal" },
 ];
 
 export default function Footer() {
@@ -23,14 +27,14 @@ export default function Footer() {
           className="flex items-center justify-between p-6 lg:px-8"
         >
           <div className="flex lg:flex-1 flex-row">
-            <a href="/" className="-m-1.5 p-1.5 flex">
+            <a href="/" className="-m-1.5 p-1.5 flex items-center">
               <span className="sr-only">takacsmark.com</span>
               <img
                 alt="takacsmark.com"
                 src={siteLogo.src}
                 className="h-8 w-auto rounded-md"
               />
-              <span className="ml-4 font-logo font-black text-l">
+              <span className="ml-4 font-logo font-black text-l item">
                 takacsmark.com
               </span>
             </a>
@@ -51,8 +55,14 @@ export default function Footer() {
                 key={item.name}
                 href={item.href}
                 className="text-sm/6 font-semibold text-gray-900"
+                target={item.type === "external" ? "_blank" : "_self"}
               >
-                {item.name}
+                <div className="flex flex-row items-center gap-1">
+                  {item.name}
+                  {item.type === "external" && (
+                    <ArrowTopRightOnSquareIcon className="size-4" />
+                  )}
+                </div>
               </a>
             ))}
           </div>
@@ -94,8 +104,14 @@ export default function Footer() {
                       key={item.name}
                       href={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                      target={item.type === "external" ? "_blank" : "_self"}
                     >
-                      {item.name}
+                      <div className="flex flex-row items-center gap-1">
+                        {item.name}
+                        {item.type === "external" && (
+                          <ArrowTopRightOnSquareIcon className="size-4" />
+                        )}
+                      </div>
                     </a>
                   ))}
                 </div>
