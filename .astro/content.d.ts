5189d54fa266186bc3ce2741e23408ddae008a8f
Mark Takacs <takacs.mark@icloud.com>
2026-03-06 17:34:12 +0100

    clean

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -164,11 +164,9 @@ declare module 'astro:content' {
 	type DataEntryMap = {
 		"blog": Record<string, {
   id: string;
-  render(): Render[".md"];
-  slug: string;
-  body: string;
+  body?: string;
   collection: "blog";
-  data: InferEntrySchema<"blog">;
+  data: any;
   rendered?: RenderedContent;
   filePath?: string;
 }>;
@@ -204,6 +202,6 @@ declare module 'astro:content' {
 		LiveContentConfig['collections'][C]['loader']
 	>;
 
-	export type ContentConfig = typeof import("../src/content.config.js");
+	export type ContentConfig = never;
 	export type LiveContentConfig = never;
 }
