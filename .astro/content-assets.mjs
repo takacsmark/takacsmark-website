5ad9e6642a69cc3dbf461ceb308322840bbddfb7
Mark Takacs <takacs.mark@icloud.com>
2026-03-06 17:42:52 +0100

    untrack .astro

--- .astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_20YcLK from "./savalera-agents-hub.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2026-03-05-moving-to-coding-factory%2Findex.md";
-export default new Map([["./savalera-agents-hub.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2026-03-05-moving-to-coding-factory%2Findex.md", __ASTRO_IMAGE_IMPORT_20YcLK]]);
-		
